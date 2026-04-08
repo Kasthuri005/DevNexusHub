9ef1ba0b9fb61fc7ee5e56ede3393baa848f93a6
Kasthuri M <manikasthuri2019@gmail.com>
2026-04-08 09:40:07 +0530

    Delete myproject3 directory

--- myproject3/accounts/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms 
- 
-class CustomLoginForm(forms.Form): 
-    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'})) 
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'})) 
--- myproject3/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('', views.home, name='admin'),
-    path('home/', views.home, name='home'),
-path('restricted/', views.restricted_view, name='restricted_page')
-]
