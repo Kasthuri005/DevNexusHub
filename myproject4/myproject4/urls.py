522e9bfaf4eb4b9cbd64d39b8431d04a7e54ba59
Kasthuri M <manikasthuri2019@gmail.com>
2026-04-08 09:40:19 +0530

    Delete myproject4 directory

--- myproject4/myproject4/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app4 import views
-
-urlpatterns = [
-    path('', views.feedback_view, name='feedback_form'),
-    path('feedback/list/', views.feedback_list, name='feedback_list'),
-]
