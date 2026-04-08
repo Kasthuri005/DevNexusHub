260a69191a4b29350fd3099f5078df1d8bd721ac
Kasthuri M <manikasthuri2019@gmail.com>
2026-04-08 09:39:45 +0530

    Delete ex9 directory

--- ex9/ex9/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Product listing page
-    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),  # Add product to cart
-    path('cart/', views.cart, name='cart'),  # View cart page
-    path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),  # Remove from cart
-]
